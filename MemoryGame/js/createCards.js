68aad6b83a2249edd36bb489a46d60b5bcd68dba
Giulia Nencini <giulianencini92@gmail.com>
2018-01-21 23:06:02 +0100

    first commit

---
+++ MemoryGame/js/createCards.js
@@ -0,0 +1,110 @@
+var background="C:/Users/Giulia/Universita'/IUM/GIULIA FINAL 2018/images/rainbow01.jpg";
+
+function my_3x4(){
+	var box=document.getElementById('table');
+	var w=box.style.width;
+	var h=box.style.height;
+	console.log(w);
+	console.log(h);
+	
+	var wInt = parseInt(document.getElementById('table').style.width);
+	var hInt = parseInt(document.getElementById('table').style.height);
+	var lato = hInt/4 - 5;
+	
+	var id=0;
+	
+	for(x=0; x<3;x++) {
+		var br = document.createElement('div');
+		br.style.width=lato;
+		br.style.height=lato+20;
+		br.className = "space";
+		for(y=0; y<4;y++) {
+			var vertical = document.createElement('img');
+			vertical.style.width=lato;
+		    vertical.style.height=lato;
+		    vertical.id=id;
+		    vertical.style.marginTop = "33px";
+		    vertical.style.marginLeft = "120px";
+		    vertical.style.cursor = "pointer";
+			id++;
+			vertical.src=background;
+		    vertical.className = "cards";
+	    	document.getElementById('table').appendChild(vertical);
+		}document.getElementById('table').appendChild(br);
+	}
+}
+
+function my_4x5(){
+	var box=document.getElementById('table');
+	var w=box.style.width;
+	var h=box.style.height;
+	console.log(w);
+	console.log(h);
+	
+	var wInt = parseInt(document.getElementById('table').style.width);
+	var hInt = parseInt(document.getElementById('table').style.height);
+	var lato = hInt/5 - 10;
+
+	var id=0;
+	
+	for(x=0; x<4;x++) {
+		var br = document.createElement('div');
+		br.style.width=lato;
+		br.style.height=lato+20;
+		br.className = "space";
+		for(y=0; y<5;y++) {
+			var vertical = document.createElement('img');
+			vertical.style.width=lato;
+		    vertical.style.height=lato;
+	   		vertical.id=id;
+	   		vertical.style.marginTop = "27px";
+		    vertical.style.marginLeft = "100px";
+		    vertical.style.cursor = "pointer";
+			id++;
+			vertical.src=background;
+		    
+		   	vertical.className = "cards";
+	    	document.getElementById('table').appendChild(vertical);
+		}document.getElementById('table').appendChild(br);
+	}
+}
+
+function my_5x6(){
+	var box=document.getElementById('table');
+	var w=box.style.width;
+	var h=box.style.height;
+	console.log(w);
+	console.log(h);
+	
+	var wInt = parseInt(document.getElementById('table').style.width);
+	var hInt = parseInt(document.getElementById('table').style.height);
+	var lato = hInt/6 - 10;
+	
+	var id=0;
+	
+	for(x=0; x<5;x++) {
+		var br = document.createElement('div');
+		br.style.width=lato;
+		br.style.height=lato+20;
+		br.className = "space";
+		for(y=0; y<6;y++) {
+			var vertical = document.createElement('img');
+			vertical.id=id;
+			id++;
+			vertical.style.width=lato;
+		    vertical.style.height=lato;
+		    vertical.style.marginTop = "20px";
+		    vertical.style.marginLeft = "86px";
+		    vertical.style.cursor = "pointer";
+		    vertical.className = "cards";
+			vertical.src=background;
+		    
+	    	document.getElementById('table').appendChild(vertical);
+		}document.getElementById('table').appendChild(br);
+	}
+
+	
+
+}
+
+
---
+++ MemoryGame/js/windowUtils.js
@@ -0,0 +1,89 @@
+// Registrazione delle azioni da fare alla chiusura delle box e al riempimento del canvas 
+
+window.onload = function(){
+	document.getElementById('close').onclick = function(){
+        
+        document.getElementById("boxAbout").style.display="none";
+        document.getElementById("boxHelp").style.display="none";
+        document.getElementById("boxOptions").style.display="none";
+        document.getElementById("firstPage").style.opacity="1";
+        enableAllButtonAbove();
+        return false;
+    	};
+    
+    document.getElementById('close2').onclick = function(){
+        document.getElementById("boxAbout").style.display="none";
+        document.getElementById("boxHelp").style.display="none";
+        document.getElementById("boxOptions").style.display="none";
+        document.getElementById("firstPage").style.opacity="1";
+        enableAllButtonAbove();
+        return false;
+    	};
+    
+    document.getElementById('close3').onclick = function(){
+        document.getElementById("boxAbout").style.display="none";
+        document.getElementById("boxHelp").style.display="none";
+        document.getElementById("boxOptions").style.display="none";
+        document.getElementById("firstPage").style.opacity="1";
+        enableAllButtonAbove();
+        return false;
+    	};
+
+    document.getElementById('close4').onclick = function(){
+        var answer = confirm ("Sei sicuro di voler abbandonare la partita?")
+        if (answer){
+            document.getElementById("boxAbout").style.display="none";
+            document.getElementById("boxHelp").style.display="none";
+            document.getElementById("boxOptions").style.display="none";
+            document.getElementById("boxGame").style.display="none";
+            //document.getElementById("via").style.display="none";
+            document.getElementById("firstPage").style.opacity="1";
+            clearDiv();
+            enableAllButtonAbove();
+            return false; 
+            }
+    	};
+    
+    document.getElementById('close5').onclick = function(){
+        
+            document.getElementById("boxAbout").style.display="none";
+            document.getElementById("boxHelp").style.display="none";
+            document.getElementById("boxOptions").style.display="none";
+            document.getElementById("ciao").style.display="none";
+            document.getElementById("time").style.display="none";
+            document.getElementById("fine").style.display="none";
+            document.getElementById("star1").style.display="none";
+            document.getElementById("star2").style.display="none";
+            document.getElementById("star3").style.display="none";
+            document.getElementById("star1").style.display="&star;";
+            document.getElementById("star2").style.display="&star;";
+            document.getElementById("star3").style.display="&star;";
+            document.getElementById("time").innerHTML="Hai completato il memory in: "; 
+            document.getElementById("firstPage").style.opacity="1";
+            clearDiv();
+            enableAllButtonAbove();
+        };
+
+    document.getElementById('restart').onclick = function(){
+
+        document.getElementById("boxAbout").style.display="none";
+        document.getElementById("boxHelp").style.display="none";
+        document.getElementById("boxOptions").style.display="none";
+        document.getElementById("fine").style.display="none";
+        document.getElementById("ciao").style.display="none";
+        document.getElementById("time").style.display="none";
+        document.getElementById("star1").style.display="none";
+        document.getElementById("star2").style.display="none";
+        document.getElementById("star3").style.display="none";
+        document.getElementById("star1").style.display="&star;";
+        document.getElementById("star2").style.display="&star;";
+        document.getElementById("star3").style.display="&star;";
+        document.getElementById("time").innerHTML="Hai completato il memory in: "; 
+        document.getElementById("firstPage").style.opacity="1"; 
+        clearDiv();
+        myStartAfterSettingsFunction();
+    }
+
+    fillCanvas();
+
+};
